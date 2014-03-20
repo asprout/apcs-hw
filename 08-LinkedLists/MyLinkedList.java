a39e865f116bf2b2c978b8bd0f16a34a6228f38e
Ling Dong <lingd1944@gmail.com>
2014-03-20 03:16:00 -0400

    added 08

---
+++ 08-LinkedLists/MyLinkedList.java
@@ -0,0 +1,82 @@
+public class MyLinkedList {
+    private Node head;
+
+    public MyLinkedList(){
+	head = null;
+    }
+    
+    public void add(String s){
+	Node tmp = new Node(s);
+	tmp.setNext(head);
+	head = tmp;
+    }
+    public String toString(){
+	String s = "";
+	Node temp = head;
+	while (temp != null){
+	    s += temp.getData()+ " ";
+	    temp = temp.getNext();
+	}
+	return s;
+    }
+    public void add(int i, String s){
+	if (i==0){
+	    add(s);
+	}
+	else {
+	    Node temp = head;
+	    for (int x = 0; x < i - 1; x++){
+		temp = temp.getNext();
+	    }
+	    Node n = new Node(s);
+	    n.setNext(temp.getNext());
+	    temp.setNext(n);
+	}
+    }
+    public String get(int i){
+	Node temp = head;
+	for (int c = 0; c<i; c++){
+	    temp = temp.getNext();
+	}
+	return temp.getData();
+    }
+    public String remove(int i){
+	Node temp = head;
+	Node o;
+	if (i==0){
+	    o = head;
+	    head = head.getNext();
+	}
+	else {
+	    i = i + 1;
+	    for (int x = 0; x < i - 2; x++ ){
+		temp = temp.getNext();
+	    }
+	    o = temp.getNext();
+	    temp.setNext(o.getNext());
+	}
+	return o.getData();
+    }
+    public int find(String s){
+	Node temp = head;
+	int i = 0;
+	while (temp != null){
+	    if (temp.getData().equals(s)){
+		return i;
+	    }
+	    temp = temp.getNext();
+	    i++;
+	}
+	return -1;
+    }
+    public int length(){
+	Node temp = head;
+	int i = 0;
+	while (temp!= null){
+	    temp = temp.getNext();
+	    i++;
+	}
+	return i;
+    }
+
+}
