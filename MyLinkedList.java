ad74316fc53c2f346f3603f209b3451cfc4a4288
Ling Dong <lingd1944@gmail.com>
2014-03-24 08:39:32 -0400

    doesn't matter

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,15 +1,24 @@
 public class MyLinkedList {
     private Node head;
+    private Node tail;
+    private int len;
 
     public MyLinkedList(){
 	head = new Node("");
+	tail = new Node("");
+	len = 0;
     }
     
     public void add(String s){
 	Node tmp = new Node(s);
-	tmp.setNext(head);
-	head = tmp;
+	Node t2 = head.getNext();
+	for (int i = 0; i < len; i++){
+	    t2 = t2.getNext();
+	}
+	t2.setNext(tmp);
+	tmp.setNext(tail);
     }
+
     public String toString(){
 	String s = "";
 	Node temp = head;
@@ -20,43 +29,44 @@ public class MyLinkedList {
 	return s;
     }
     public void add(int i, String s){
-	if (i==0){
-	    add(s);
+	if (i>len){
+	    return ;
 	}
 	else {
-	    Node temp = head;
-	    for (int x = 0; x < i - 1; x++){
+	    Node temp = head.getNext();
+	    Node t2 = head;
+	    for (int x = 0; x < i; x++){
+		t2 = temp;
 		temp = temp.getNext();
 	    }
 	    Node n = new Node(s);
-	    n.setNext(temp.getNext());
-	    temp.setNext(n);
+	    t2.setNext(n);
+	    n.setNext(temp);
+	    len = len + 1;
 	}
     }
+
     public String get(int i){
-	Node temp = head;
+	Node temp = head.getNext();
 	for (int c = 0; c<i; c++){
 	    temp = temp.getNext();
 	}
 	return temp.getData();
     }
+
     public String remove(int i){
-	Node temp = head;
-	Node o;
-	if (i==0){
-	    o = head;
-	    head = head.getNext();
+	Node temp = head.getNext();
+	Node t2 = head;
+	for (int l = 0; l < i; l++){
+	    t2 = temp;
+	    temp = temp.getNext();
 	}
-	else {
-	    i = i + 1;
-	    for (int x = 0; x < i - 2; x++ ){
-		temp = temp.getNext();
-	    }
-	    o = temp.getNext();
-	    temp.setNext(o.getNext());
-	}
-	return o.getData();
+	t2.setNext(temp.getNext());
+	String old = tmp.getData();
+	len = len - 1;
+	return old;
     }
+
     public int find(String s){
 	Node temp = head;
 	int i = 0;
