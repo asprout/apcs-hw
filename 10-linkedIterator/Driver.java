501aae3628980571486ba5c669fb7afc621774a7
asprout <lingd1944@gmail.com>
2014-03-27 03:22:47 -0400

    updated 3/27

--- 10-linkedIterator/Driver.java
+++ 10-linkedIterator/Driver.java
@@ -10,5 +10,18 @@ public class Driver {
 	System.out.println(a);
 	a.add(3, "four");
 	System.out.println(a);
+	System.out.println(a.length());
+	System.out.println(a.get(2));
+	System.out.println(a.find("tew"));
+	System.out.println("\nTop answers should be: 4, tree, 1");
+
+
+	//ITERATOR TESTING
+
+	
+	myLLIterator<String> it = a.iterator();
+	System.out.println(it.hasNext());
+	System.out.println(it.next());
+	
     }
 }
