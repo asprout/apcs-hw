5dbd9333e943495145e87d64578c21d4ce42fca1
Ling Dong <lingd1944@gmail.com>
2014-04-02 08:20:42 -0400

    okay

---
+++ 10-linkedIterator/Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+
+    public static void main(String[] args){
+
+	MyLinkedList l = new MyLinkedList();
+
+	myIterator<String> it = s.myIterator();
+
+    }
+}
---
+++ 10-linkedIterator/myIterator.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class myIterator<E> implements Iterator<E>{
+
+    public boolean hasNext(){
+    }
+
+    public E next(){
+    }
+
+    public void remove(){
+    }
+
+}
