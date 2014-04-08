22c820bdb1ce056cf950449fabc2e9cd78e2eae6
Ling Dong <lingd1944@gmail.com>
2014-04-07 23:50:11 -0400

    updateS

---
+++ 13-RPN/Calculator.java
@@ -0,0 +1,16 @@
+// if */+- pop twice, calc and push
+// else just push
+// use scanner to read string
+// use doubles
+
+import java.io.*;
+import java.util.*;
+
+public class Calculator {
+    
+    public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int i = sc.nextInt();
+ 
+   }
+}
